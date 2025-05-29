ac7e94cfa8f957d72d562906600609a62dde171e
Sahril <103849124+CallMeSahril@users.noreply.github.com>
2025-05-30 02:57:25 +0700

    a

--- controllers/dashboard_controller.py
+++
@@ -1,5 +0,0 @@
-from flask import render_template
-
-class DashboardController:
-    def index(self):
-        return render_template('dashboard.html', title="Dashboard Admin")
--- routes/dashboard_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from controllers.dashboard_controller import DashboardController
-
-dashboard_bp = Blueprint('dashboard', __name__)
-controller = DashboardController()
-
-@dashboard_bp.route('/')
-def dashboard():
-    return controller.index()
